0fe6b78f5870461ffe5986ce0f927c10ad3b8a03
g4wwh <phil@pavelin.com>
2020-05-13 17:11:44 +0100

    update

---
+++ source/form-to-email.php
@@ -0,0 +1,22 @@
+<?php
+header("location:form_thanks");
+
+
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = "Name: " . $name . "\n";
+  $message .= "Email: " . "mailto:" . $email . "\n";
+  $message .= "Subject: " . $subject . "\n"; 
+  $message .= "Message:\n" . $_POST['message'];
+  $to = "philip.pavelin@stfc.ac.uk";
+#  $to = "phil@pavelin.com";
+#  $to = "scarf@hpc-support.rl.ac.uk";
+  $mail_headers = "From: $email \r\n";
+
+  $mail_headers .= "Reply-To: $email \r\n";
+
+  mail($to,$subject,$message,$mail_headers);
+
+
+?>
