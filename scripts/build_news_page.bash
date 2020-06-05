4036cdfc572eef5323c5cbce375797313a2b8e79
root <root@phil1.esc.rl.ac.uk>
2020-06-05 20:38:04 +0100

    new feed

---
+++ scripts/build_news_page.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NEWS_TEMPLATE=/sphinx/scarf/build/html/scarf_news_template.html
+NEWS_PAGE=/sphinx/scarf/build/html/scarf_news.html
+RSS_FEED=/sphinx/scarf/scripts/tmp/rss_feed.html
+
+cp -f $NEWS_TEMPLATE $NEWS_PAGE
+
+/usr/bin/python /sphinx/scarf/scripts/rss2html.py > $RSS_FEED
+
+sed -i -e "/taken from the SCARF NEWS Mailing List RSS feed/r $RSS_FEED" $NEWS_PAGE 
--- scripts/rss2html.py
+++ scripts/rss2html.py
@@ -13,11 +13,20 @@ if hasattr(ssl, '_create_unverified_context'):
 
 feed = feedparser.parse(url)
 
+#print ("document.write('\\")
+
 for news_item in feed.entries:
 
   date = "(%d/%02d/%02d)" % (news_item.published_parsed.tm_year,\
     news_item.published_parsed.tm_mon, \
     news_item.published_parsed.tm_mday)
 
-  print ("`" + date + " ..... " + news_item.title + " <" + news_item.link + ">`_")
-  print ("")
+#  print ("`" + date + " ..... " + news_item.title + " <" + news_item.link + ">`_")
+#  print ("")
+
+
+
+  print (date + "..... <a href=" + news_item.link + ">" + news_item.title + "</a>")
+  print ("<br><br>")
+
+#print ("');")
